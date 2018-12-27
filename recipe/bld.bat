c9e2537c0b73c382313ccb4d19d38dfc06112cee
Larry Shaffer <lshaffer@boundlessgeo.com>
2018-12-27 15:03:53 -0700

    Working Win recipe

---
+++ recipe/bld.bat
@@ -0,0 +1,45 @@
+:: Spoof OSGEO4W location in Find*.cmake modules
+set OSGEO4W_ROOT=%LIBRARY_PREFIX%
+set OSGEO4W_HOME=%LIBRARY_PREFIX%
+
+mkdir builddir
+cd builddir
+
+cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+    -G "Ninja" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DWITH_GDAL:BOOL=ON ^
+    -DWITH_GEOTIFF:BOOL=ON ^
+    -DWITH_LASZIP:BOOL=ON ^
+    -DWITH_STATIC_LASZIP:BOOL=FALSE ^
+    -DWITH_UTILITIES:BOOL=TRUE ^
+    -DWITH_TESTS:BOOL=ON ^
+    -DBUILD_OSGEO4W:BOOL=FALSE ^
+    ..
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+cmake --build .
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+cmake --build . --target test
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+cmake --build . --target install
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+:: TODO: Install a wrapper script for .exe launches?
+::       See liblas-osgeo4w-*.bat in src
+
+
+:: -DBoost_INCLUDE_DIR:PATH=%LIBRARY_INC% ^
+
+:: -DBOOST_INCLUDEDIR=%BOOST% ^
+:: -DTIFF_INCLUDE_DIR=%OSGEO4W%\include ^
+:: -DTIFF_LIBRARY=%OSGEO4W%\lib\libtiff_i.lib ^
+:: -DGEOTIFF_INCLUDE_DIR=%OSGEO4W%\include ^
+:: -DGEOTIFF_LIBRARY=%OSGEO4W%\lib\geotiff_i.lib ^
+:: -DGDAL_INCLUDE_DIR=%OSGEO4W%\include ^
+:: -DGDAL_LIBRARY=%OSGEO4W%\lib\gdal_i.lib ^
+:: -DLASZIP_INCLUDE_DIR=%OSGEO4W%\include ^
+:: -DLASZIP_LIBRARY=%OSGEO4W%\lib\laszip.lib ^
