bc226503401143e55ab3b62b5c317a6460e03ca6
Larry Shaffer <lshaffer@boundlessgeo.com>
2018-12-27 15:03:34 -0700

    Add files from kitware-geospatial-recipes

---
+++ recipe/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir build && cd build
+
+# Make libraries
+cmake -G "Ninja" \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_PREFIX_PATH=${PREFIX} \
+  -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
+  -DBoost_INCLUDE_DIR:PATH=${PREFIX}/include \
+  -DWITH_TESTS:BOOL=OFF \
+  ${SRC_DIR}
+
+# compile & install
+ninja install
+
