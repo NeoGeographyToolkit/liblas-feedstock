1e77cf1011a8cfa44546e87935a3b7ccbc40235c
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2020-06-07 16:30:19 -0700

    Minor recipe tweaks

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,14 +5,23 @@ set -e
 mkdir build && cd build
 
 # Make libraries
-cmake -G "Ninja" \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_PREFIX_PATH=${PREFIX} \
-  -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
-  -DBoost_INCLUDE_DIR:PATH=${PREFIX}/include \
-  -DWITH_TESTS:BOOL=OFF \
+cmake                                          \
+  -DCMAKE_BUILD_TYPE=Release                   \
+  -DCMAKE_PREFIX_PATH=${PREFIX}                \
+  -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX}        \
+  -DBoost_INCLUDE_DIR:PATH=${PREFIX}/include   \
+  -DWITH_LASZIP=true                           \
+  -DLASZIP_INCLUDE_DIR:PATH=${PREFIX}/include  \
+  -DWITH_TESTS:BOOL=OFF                        \
+  -DWITH_GDAL:BOOL=ON                          \
+  -DGDAL_INCLUDE_DIR:PATH=${PREFIX}/include    \
+  -DWITH_GEOTIFF:BOOL=ON                       \
+  -DGEOTIFF_INCLUDE_DIR:PATH=${PREFIX}/include \
+  -DTIFF_INCLUDE_DIR:PATH=${PREFIX}/include    \
+  -DBUILD_SHARED_LIBS=ON                       \
+  -DCMAKE_VERBOSE_MAKEFILE=ON                  \
   ${SRC_DIR}
 
-# compile & install
-ninja install
+make -j $CPU_COUNT
+make install
 
